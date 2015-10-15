e2a7ba9d0cf5e345c91344b8816d73f99e6d9539
Caroline Marcks <carolinem@meetup.com>
2015-10-15 13:43:01 -0400

    start a scalatra server directory

---
+++ scalatra/lunchup/src/main/scala/com/lunchup/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package com.lunchup
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends LunchupStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
