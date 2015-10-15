2f744b037dc8b0c82af7464ef0ba665bb469aae2
Caroline Marcks <carolinem@meetup.com>
2015-10-15 14:33:10 -0400

    get scalatra to state that prev server was at

--- scalatra/lunchup/project/build.scala
+++ scalatra/lunchup/project/build.scala
@@ -28,7 +28,12 @@ object LunchupBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+        "org.apache.commons" % "commons-io" % "1.3.2",
+        "org.apache.commons" % "commons-jexl" % "2.1.1",
+        "org.apache.commons" % "commons-lang3" % "3.0",
+        "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4.2",
+        "org.json4s" %% "json4s-native" % "3.3.0"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
